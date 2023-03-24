fefd8a61038214f26db0ad74be4182a1c3d24ad8
Natasha Machado <natashasantosmachado@gmail.com>
2023-03-23 19:24:10 -0700

    start of the class

---
+++ ChessProject/Classes.swift
@@ -0,0 +1,13 @@
+//
+//  Classes.swift
+//  ChessProject
+//
+//  Created by Natasha on 3/23/23.
+//
+
+import Foundation
+
+
+class Piece {
+  
+}
---
+++ ChessProject/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ChessProject
+//
+//  Created by Natasha on 3/23/23.
+//
+
+import Foundation
+
+
+
