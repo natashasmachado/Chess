015340b9f266ab84b4a4ec6e1f15d3e4a0822147
Natasha Machado <natashasantosmachado@gmail.com>
2023-03-23 21:15:10 -0700

    classes and methodes

--- ChessProject/Classes.swift
+++ ChessProject/Classes.swift
@@ -1,5 +1,5 @@
 //
-//  Classes.swift
+//  main.swift
 //  ChessProject
 //
 //  Created by Natasha on 3/23/23.
@@ -8,6 +8,116 @@
 import Foundation
 
 
-class Piece {
+class Piece: CustomStringConvertible , Equatable {
+  private(set) var value: Int
+  private(set) var isWhite: Bool
+  var description: String {
+    return "Piece (value = \(value))"
+  }
+  
+  init(value: Int, isWhite: Bool) {
+    self.value = value
+    self.isWhite = isWhite
+  }
+  
+  func getter() -> Int {
+    return value
+  }
+  
+  func setter(value: Int) {
+    self.value = value
+  }
+  
+  func move() -> Void {
+    
+  }
+  
+  static func == (lhs: Piece, rhs: Piece) -> Bool {
+    return lhs.value == rhs.value && lhs.isWhite == rhs.isWhite
+  }
   
 }
+
+class King: Piece {
+  override func move() {
+    print("One square")
+  }
+  
+  override var description: String {
+    return "King (value = \(value))"
+  }
+}
+
+class Queen: Piece {
+  override func move() {
+    print("Like bishop and rook")
+  }
+  
+  override var description: String {
+    return "Queen (value = \(value))"
+  }
+}
+
+class Rook: Piece {
+  override func move() {
+    print("Horizontally or vertically")
+  }
+  
+  override var description: String {
+    return "Rook (value = \(value))"
+  }
+}
+
+class Bishop: Piece {
+  override func move() {
+    print("Diagonally")
+  }
+  
+  override var description: String {
+    return "Bishop (value = \(value))"
+  }
+}
+
+class Knight: Piece {
+  override func move() {
+    print("Like an L")
+  }
+  
+  override var description: String {
+    return "Knight (value = \(value))"
+  }
+}
+
+class Pawn: Piece {
+  var promoted: Bool = false
+  var newPiece: Piece?
+  
+  override init(value: Int, isWhite: Bool) {
+    super.init(value: value, isWhite: isWhite)
+  }
+  
+  func promote(_ newPiece: Piece) {
+    self.promoted = true
+    self.newPiece = newPiece
+  }
+  
+  override var description: String {
+           if promoted == true {
+             return "Pawn (value = \(value)) , promoted = \(promoted), newPiece = \(newPiece?.description ?? "")"
+           } else {
+             return  "Pawn (value = \(value))"
+           }
+  }
+  
+  static func == (lhs: Pawn, rhs: Pawn) -> Bool {
+    if lhs.promoted != rhs.promoted {
+      return false
+    }
+    if lhs.promoted && rhs.promoted {
+      return lhs.newPiece == rhs.newPiece
+    }
+    return lhs.value == rhs.value && lhs.isWhite == rhs.isWhite
+  }
+}
+
+
--- ChessProject/main.swift
+++ ChessProject/main.swift
@@ -9,3 +9,10 @@ import Foundation
 
 
 
+let pawn = Pawn(value: 1, isWhite: true)
+let knight = Knight(value: 2, isWhite: true)
+let bishop = Bishop(value: 3, isWhite: true)
+let rook = Rook(value: 5, isWhite: true)
+let queen = Queen(value: 9, isWhite: true)
+let king = King(value: 1000, isWhite: true)
+
